5f625bcb23e9d8806c1dbbbf163913641d3d5b00
Joel Olivero <joelolivero.student@careerdevs.com>
2020-04-12 20:55:57 -0400

    updated app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
 import React, { Component } from 'react';
 import './App.css';
+import Resource from './components/Resource'
+
+const resource = {
+      PosterName: 'Cliff',
+      resourceAuthor: 'NetNinja',
+      authorSkillLevel: 'Advanced',
+      cohort: '0',
+      title: 'React & Redux Complete',
+      category: ['React', 'Redux'],
+      summary: 'A good resource on building one page apps',
+      link: 'www.google.com/search?q=NetNinja%20React',
+      resourceType: 'Playlist',
+      datePublished: '2018-09-10.00:00.00.000z',
+      videoLength: null,
+      timeToComplete: 300,
+      rating: 5,
+      comments: [{user: 'Joe', text: 'Pretty good resource'}]    
+  }
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Welcome to BrainHive!</h1>
+          <div className='box'>
+            <Resource resource={this.resource}/>
+            <Resource resource={this.resource}/>
+            <Resource resource={this.resource}/>
+          </div>
+          
       </div>
     )
   }
---
+++ src/components/Resource.js
@@ -0,0 +1,17 @@
+import React from  'react';
+
+const Resource = ({resource}) => {
+   
+    return (
+        <div>
+            <h2>{resource.title}</h2>
+            <p>{resource.author}</p>
+            <p>Published on: {resource.datePublished}</p>
+        
+            { resource.videoLength ? (<p>Length: {resource.videoLength} minutes}</p>) : null }
+            <p>Comments: {resource.comments.length} </p>
+        </div>
+        )
+}
+
+export default Resource
