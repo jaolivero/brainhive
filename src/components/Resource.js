e057842bc47b198ebb001a3804beac9c16ee0404
Joel Olivero <joelolivero.student@careerdevs.com>
2020-04-12 22:01:16 -0400

    pushing

--- src/components/Resource.js
+++ src/components/Resource.js
@@ -8,7 +8,7 @@ const Resource = ({resource}) => {
             <p>{resource.author}</p>
             <p>Published on: {resource.datePublished}</p>
         
-            { resource.videoLength ? (<p>Length: {resource.videoLength} minutes}</p>) : null }
+            { resource.videoLength ? (<p>Length: {resource.videoLength} minutes</p>) : null }
             <p>Comments: {resource.comments.length} </p>
         </div>
         )
